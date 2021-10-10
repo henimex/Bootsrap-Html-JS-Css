d15fda1f644381f5ede4c6875596223f0ebb12ab
henimex <49072861+henimex@users.noreply.github.com>
2021-10-10 03:02:42 +0300

    Events

--- 110Plus/script_number_string_convert_process.js
+++ 110Plus/script_number_string_convert_process.js
@@ -955,3 +955,314 @@ while (left > 0) {
     console.log("Hakkınız Bitti.");
   }
 }
+
+
+//Functions
+
+function yasHesapla(dogumYili) {
+  return 2021 - dogumYili;
+}
+
+function emeklilikHesapla(dogumYili, isim) {
+  let yas = yasHesapla(dogumYili);
+  let emeklilik = 65 - yas;
+
+  if (emeklilik > 0) {
+    console.log(`Sayın ${isim}; Emekli Olmanıza ${emeklilik} yıl kaldı`);
+  } else {
+    console.log(
+      `Emekliliği yaş olarak hak etmiş bulunmaktasınız. Prim gün süreniz 160.000 doldurmus iseniz emekli olabilirsiniz.`
+    );
+  }
+  return emeklilik;
+}
+
+let val = yasHesapla(1983);
+console.log(val);
+
+emeklilikHesapla(1983, "Ferhat");
+
+//Function Declerations Expressions
+
+function sum(a, b) {
+  var c = a + b;
+  return c;
+}
+
+const sumDif = function (a, b) {
+  if (typeof a === "undefined") {
+    a = 0;
+  }
+
+  typeof b === "undefined" ? (b = 0) : false;
+
+  return a + b;
+};
+
+const sumDef = function (a = 0, b = 0) {
+  return a + b;
+};
+
+console.log(sum(10, 5));
+console.log(sumDif(10, 5));
+console.log(sumDef(20, 90));
+
+function argControl() {
+  console.log(arguments);
+}
+
+function sumAll() {
+  var total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
+}
+
+argControl(10, 50, "55", "ferhat");
+console.log(sumAll(10, 10, 5, 6, "ss"));
+
+// Demo
+
+var hesapA = { ad: "Ferhat", hesapNo: "1245451", bakiye: 2000, ekHesap: 1000 };
+var hesapB = { ad: "Dilek", hesapNo: "4561561", bakiye: 5000, ekHesap: 10000 };
+var hesapc = { ad: "Esila", hesapNo: "2474418", bakiye: 300, ekHesap: 0 };
+
+function paraCek(hesap, miktar) {
+  console.log(`Merhaba ${hesap.ad}. ${hesap.hesapNo} nolu hesap için işlem yapıalcaktır.`);
+  if (hesap.bakiye >= miktar) {
+    hesap.bakiye = hesap.bakiye - miktar;
+    console.log(
+      `Çekilmek İstenen Tutar : ${miktar} Bakiye : ${
+        hesap.bakiye
+      } paranızı alabilirsiniz.\n Hesap Bilgisi: ${
+        hesap.bakiye - miktar
+      } TL \n Ek Hesap : ${hesap.ekHesap}`
+    );
+  } else {
+    console.log(
+      `Çekilmek İstenen Tutar : ${miktar} \n Bakiye : ${hesap.bakiye} Ek Hesap Kontrolü Yapılıyor...`
+    );
+    var toplam = hesap.bakiye + hesap.ekHesap;
+    if (toplam >= miktar) {
+      var secim = confirm(
+        "Kalan Tutarı Ek Hesabınızdan Kullanmak İstermisiniz."
+      );
+      console.log(secim)
+      if (secim) {
+        kalan = miktar - hesap.bakiye;
+        hesap.bakiye = 0;
+        hesap.ekHesap = hesap.ekHesap - kalan;
+        console.log(
+          `Bakiyenizden ${hesap.bakiye} TL Ek Hesabınızdan ise ${kalan} TL çekilmiştir.`
+        );
+        console.log(
+          `Rapor : \n Bakiye : 0 TL \n Ek Hesap : ${hesap.ekHesap} TL dir.`
+        );
+      } else if (!secim) {
+        console.log(
+          `Bakiyeniz : ${hesap.bakiye} TL \n Ek Hesabınız : ${hesap.ekHesap} TL dir. Çekme işlemi iptal edildi. iyi günler dileriz.`
+        );
+      }
+    } else {
+      console.log(`Hesaplarınzda Yeterli bakiye bulunmamaktadır. \n 
+        Bakiye : ${hesap.bakiye} TL\n
+        Ek Hesap : ${hesap.ekHesap} TL \n
+        Çekilmek istenene Tutar : ${miktar} TL. İyi Günler Dileriz.`);
+    }
+  }
+}
+
+paraCek(hesapA, 2500);
+paraCek(hesapA, 2500);
+paraCek(hesapA, 1000);
+
+//Window Object
+
+let val;
+var a = 10;
+val = window;
+
+function abc() {
+  return 0;
+}
+
+//var b = confirm("Are you sure you want to do this.")
+var x = window.scrollX
+var y = window.scrollY
+
+console.log(val);
+console.log(x , y);
+
+// var location = window.location.href
+// window.location.reload(false)
+// console.log(location);
+let isRedirected = sessionStorage.getItem('isRedirected'); if (!isRedirected) { sessionStorage.setItem('isRedirected', true); window.location.reload(true); }
+
+let browser = window.navigator
+let doc = window.document
+let header = document.getElementById('header')
+console.log(browser)
+console.log(doc)
+console.log(header)
+
+//DOM - 1 New Things Fresh Information Knowladge Intel... AWESOME
+
+// genel not getElemets e ulasıp islem yapabilmek icin sadece for methodu kullanılabilir getElements bir html collection doner. querySelectorAll NodeList dondugu icin forEach destekliyor
+
+let element = document.getElementById("header");
+
+val = element.className;
+element.style.fontSize = "45px";
+element.style.color = "red";
+element.style.fontWeight = "bolder";
+element.innerText = "Service is nOT Runnig <em>";
+element.innerHTML = "<em> Service... I dont Know whats going on</em>";
+
+console.log(document.querySelector("#header")); //ıd ye göre secme
+console.log(document.querySelector(".selfLi").innerText); //class a gore tekli secme
+console.log(document.querySelectorAll(".selfLi")); //class a gore coklu secme NodeList doner foreach atılır
+console.log(document.querySelectorAll(".selfLi")[2].innerText); //class a gore NodeList içinden secme
+
+document.querySelector("li").style.color = "red"; //bulunan ilk elemana uygular
+document.querySelector("li:last-child").style.color = "blue"; //son elemana uygular
+document.querySelector("li:nth-child(3)").style.color = "orange"; //belirtilen indexteki elemana erişir
+document.querySelector("li:nth-child(7)").textContent =
+  "OO Rubby... im coming for you"; //belirtilen indexteki elemana erişir
+
+document.querySelector("li").classList.add("active"); //var olan classlara ekleme yapmak için
+console.log(document.querySelector("li").classList); //mevcut class listesi
+
+//Çoklu Element Seçimleri
+
+let el1 = document.getElementsByClassName("selfLi");
+let tagName = document.getElementsByTagName("li");
+let el2 = (el1[1].style.fontSize = "25px"); // yukardaki tip ve icerik degistirme islemlerinin tamamı getten sonra index numarası ile elemente ulasarakta yapılabilir.
+let aChildCard = document.getElementById("alinacakBelirlenmisAlan") //dıdısının dıdısına erişmek için sayfadaki tüm a lara degilde card altındaki a lara ornegin ıd yerine class vs de olabilir.
+//let aChildCardChild = aChildCard.getElementsByTagName('a')
+
+let tekilOlanLiler = document.querySelectorAll("li:nth-child(odd)") //farklı garip değişik olarakta çevrilir odd
+let qall = document.querySelectorAll("li");
+console.log(qall)
+
+qall.forEach((element,index)=> {
+    element.textContent = `${index} - test`
+});
+
+
+console.log(el1);
+console.log(el2);
+
+for (let i = 0; i < el1.length; i++) {
+  el1[i].style.fontSize = "45px";
+  el1[i].textContent =
+    el1[i].textContent + " for dongusu ile degistirildi. " + i;
+  el1[i].style.fontSize = "20px";
+}
+
+console.log("Id ile alınan element :" , element);
+console.log("Yukardaki elementin class adı :" , val);
+console.log("Tag name e gore alınmıs elementler : " , tagName);
+
+
+//DOM - 2
+
+let val;
+
+let list = document.querySelectorAll("li");
+
+val = list.childNodes; //:nodelist
+val = list.children; //:html collection
+val = list.parentElement;
+val = list.parentNode;
+val = list.childElementCount;
+
+list.children[0].nextSibling;
+list.children[0].nextElementSibling;
+
+list.children[0].previousSibling;
+list.children[0].previousElementSibling;
+
+
+for (let i = 0; i< list.length; i++) {
+  const element = array[i];
+    }
+
+val = list
+
+for (let i = 0; i < list.length; i++) {
+    console.log(list[i].nodeType)
+    
+}
+console.log(val);
+/*
+Node Types:
+1	Element	        Represents an element
+2	Attr	        Represents an attribute
+3	Text	        Represents textual content in an element or attribute
+4	CDATASection	Represents a CDATA section in a document (text that will NOT be parsed by a parser)
+*/
+
+//DOM - 3
+
+const taskList = document.querySelector("#ol1");
+//taskList.remove();
+//taskList.childNodes[1].remove();
+//taskList.children[1].removeAttribute('class');
+// const h2 = document.createElement('h2');
+// h2.setAttribute('class', 'card-header');
+// h2.appendChild(document.createTextNode('New Text'));
+
+let classlist = taskList.classList
+classlist.add('newTestList')
+classlist.remove('newTestList')
+
+let att2 = document.querySelectorAll("li");
+//let attribute = att2[0].children.getAttribute('href')
+
+
+console.log(taskList);
+console.log(classlist);
+console.log(att2[0].childNodes[1].getAttribute('href'));
+//console.log(attribute);
+
+
+//Event - Mouse
+
+const btn = document.querySelector("#btnDeleteAll");
+const btnAdd = document.querySelector("#btnAdd");
+const btnEventTester = document.querySelector("#btnEventTester");
+const liler = document.querySelector("#ol1");
+
+btn.addEventListener("click", () => {
+  console.log("Delete All Clicked");
+});
+
+//btnAdd.addEventListener
+
+btn.addEventListener("click", btnCl);
+btn.addEventListener("mouseenter", eventHandler);
+btn.addEventListener("mouseleave", eventHandler);
+btnAdd.addEventListener("mouseover", hover);
+btnEventTester.addEventListener("click", eventHandler);
+btnEventTester.addEventListener("dblclick", eventHandler);
+btnEventTester.addEventListener("mousedown", eventHandler);
+liler.addEventListener("click", eventHandler);
+liler.addEventListener("mousemove", eventHandler);
+
+function btnCl() {
+  console.log("Method : Delete All Clicked");
+  //e.preventDefault();
+}
+
+function hover(e) {
+  console.log("Method : Butonun Üzerine Gelindi");
+  console.log(e);
+  console.log(e.offsetX, e.offsetY);
+  e.preventDefault();
+}
+
+function eventHandler(event) {
+  console.log(`Event Type : ${event.type}`);
+  
+}
