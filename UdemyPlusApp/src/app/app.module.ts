eaebd356127fafe234b72e758eccb7ee29951137
henimex <49072861+henimex@users.noreply.github.com>
2021-10-16 11:55:09 +0300

    update

--- UdemyPlusApp/src/app/app.module.ts
+++ UdemyPlusApp/src/app/app.module.ts
@@ -5,13 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './Components/todo/todo.component';
-import { MovieComponent } from './Components/movie/movie.component';
+
+import { MoviesComponent } from './Components/MovieComps/movies/movies.component';
+import { MovieComponent } from './Components/MovieComps/movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    MovieComponent
+    MovieComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
